889ba8af28b1d2986d075025fc033eb073766602
gerry-sheva <gerry.sheva@proton.me>
2024-05-16 03:18:06 +0700

    stubbed card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import NavBar from './components/NavBar'
 import CartButton from './components/CartButton'
+import ProductCard from './components/Card'
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col px-4">
 			<NavBar />
-			<section className="grid grid-cols-2 gap-2]"></section>
+			<section className="grid grid-cols-2 gap-2]">
+				<ProductCard />
+			</section>
 			<CartButton />
 		</main>
 	)
