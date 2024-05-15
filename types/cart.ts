0b92b478070effebec492d239dcc1871ed360470
gerry-sheva <gerry.sheva@proton.me>
2024-05-15 21:20:49 +0700

    implemented cart :)

--- types/cart.ts
+++ types/cart.ts
@@ -1,14 +1,14 @@
-import { Product } from "./product";
+import { Product } from './product'
 
 export interface CartItem {
-  productId: Product;
-  quantity: number;
+	product: Product
+	quantity: number
 }
 
 export interface ShoppingCart {
-  items: CartItem[];
-  addItem(product: Product, quantity: number): void;
-  removeItem(productId: number): void;
-  updateItemQuantity(productId: number, quantity: number): void;
-  getTotalPrice(): number;
+	items: CartItem[]
+	addItem(product: Product, quantity: number): void
+	removeItem(productId: number): void
+	updateItemQuantity(productId: number, quantity: number): void
+	getTotalPrice(): number
 }
